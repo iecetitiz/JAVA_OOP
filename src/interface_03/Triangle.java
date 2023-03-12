471e7e5052c2cd1c55f2101082bc888f9400b94b
iecetitiz <i.ecetitiz@gmail.com>
2023-03-12 20:32:48 +0300

    Triangle class is added

---
+++ src/interface_03/Triangle.java
@@ -0,0 +1,13 @@
+package interface_03;
+
+public class Triangle extends GeometricalObject{
+    @Override
+    public double getArea() {
+        return 0;
+    }
+
+    @Override
+    public double getPerimeter() {
+        return 0;
+    }
+}
